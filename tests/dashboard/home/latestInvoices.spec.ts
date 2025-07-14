7c698b50c5b9a4f4173d25cce6d20b8ab9b52e3a
leninbel <leninbel@gmail.com>
2025-07-14 21:45:07 +0200

    skip next js dashboard test

--- tests/dashboard/home/latestInvoices.spec.ts
+++ tests/dashboard/home/latestInvoices.spec.ts
@@ -1,7 +1,7 @@
 import { expect } from '@playwright/test';
 import { test } from 'fixtures/fixtures';
 
-test.describe('Latest invoices block tests', () => {
+test.describe.skip('Latest invoices block tests', () => {
     test('should have correct title', async ({ page, dashboardPage }) => {
         await dashboardPage.navigate();
         expect(dashboardPage.latestInvoicesLabel).toBeVisible();
